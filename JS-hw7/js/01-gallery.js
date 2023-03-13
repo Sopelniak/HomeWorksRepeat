7c9dd49fe2754caf67e0c32374856a25eac3e625
Sopelniak <asopelnyak@gmail.com>
2023-03-14 00:20:29 +0200

    done hw7

--- JS-hw7/js/01-gallery.js
+++ JS-hw7/js/01-gallery.js
@@ -1,4 +1,55 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector("div.gallery");
+let instance;
+
+galleryEl.addEventListener("click", onClick);
+
+function onClick(e) {
+  e.preventDefault();
+  if (e.target.nodeName === "IMG") {
+    showLightbox(e.target.dataset.source);
+  }
+}
+
+function onKeyup(e) {
+  if (e.code === "Escape") {
+    instance.close();
+  }
+}
+
+function showLightbox(url) {
+  instance = basicLightbox.create(
+    `
+    <img src=${url} width="800" height="600">
+`,
+    {
+      onShow: () => {
+        document.addEventListener("keyup", onKeyup);
+      },
+      onClose: () => {
+        document.removeEventListener("keyup", onKeyup);
+      },
+    }
+  );
+  instance.show();
+}
+
+galleryEl.insertAdjacentHTML(
+  "afterbegin",
+  galleryItems
+    .map(
+      (item) => `
+        <div class="gallery__item">
+            <a class="gallery__link" href=${item.original}>
+                <img
+                    class="gallery__image"
+                    src=${item.preview}
+                    data-source=${item.original}
+                    alt=${item.description}
+                />
+            </a>
+        </div>`
+    )
+    .join("")
+);
--- JS-hw7/js/02-lightbox.js
+++ JS-hw7/js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.insertAdjacentHTML(
+  "afterbegin",
+  galleryItems
+    .map(
+      (item) => `
+        <a class="gallery__item" href=${item.original}>
+            <img 
+                class="gallery__image" 
+                src=${item.preview} 
+                alt=${item.description} 
+            />
+        </a>
+        `
+    )
+    .join("")
+);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+  scrollZoom: false,
+});
