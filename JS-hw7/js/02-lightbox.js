4de3c5405c8a0a173c946f4ada3768c9d086488e
Sopelniak <asopelnyak@gmail.com>
2023-03-17 14:48:22 +0200

    done hw7

--- JS-hw7/js/02-lightbox.js
+++ JS-hw7/js/02-lightbox.js
@@ -7,13 +7,15 @@ galleryEl.insertAdjacentHTML(
   galleryItems
     .map(
       (item) => `
-        <a class="gallery__item" href=${item.original}>
-            <img 
-                class="gallery__image" 
-                src=${item.preview} 
-                alt=${item.description} 
+        <li>
+          <a class="gallery__item" href="${item.original}">
+            <img
+              class="gallery__image"
+              src="${item.preview}"
+              alt="${item.description}"
             />
-        </a>
+          </a>
+        </li>
         `
     )
     .join("")
