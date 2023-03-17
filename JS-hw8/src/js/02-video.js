a9db9c86ef1dbf63a51f725828c6bf9ede471048
Sopelniak <asopelnyak@gmail.com>
2023-03-17 23:20:52 +0200

    task 02 done

--- JS-hw8/src/js/02-video.js
+++ JS-hw8/src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('#vimeo-player');
+const player = new Player(iframeEl);
+
+player.on(
+  'timeupdate',
+  throttle(function (e) {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+  }, 1000)
+);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
