d67f06fbb596aeec5924311701982421a990a8c2
Sopelniak <asopelnyak@gmail.com>
2023-03-17 17:08:10 +0200

    task 01 done

--- JS-hw8/src/js/01-gallery.js
+++ JS-hw8/src/js/01-gallery.js
@@ -1,5 +1,30 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.insertAdjacentHTML(
+  'afterbegin',
+  galleryItems
+    .map(
+      item => `
+        <li>
+          <a class="gallery__item" href="${item.original}">
+            <img
+              class="gallery__image"
+              src="${item.preview}"
+              alt="${item.description}"
+            />
+          </a>
+        </li>
+        `
+    )
+    .join('')
+);
+
+new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+  scrollZoom: false,
+});
